f82cded544968586103a616d550c0fd4e8a51320
park-sm <nambawon1@naver.com>
2022-08-08 20:26:40 +0900

    2022-.08-08 리우터 구현

---
+++ voca/src/App.js
@@ -0,0 +1,24 @@
+import Day from "./component/Day";
+import DayList from "./component/DayList";
+import Header from "./component/Header";
+import EmptyPage from "./component/EmptyPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        {/* <DayList />
+        <Day /> */}
+        <Routes>
+          <Route exact path="/" element={<DayList />} />
+          <Route path="/day/:day" element={<Day />} />
+          <Route path="*" element={<EmptyPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
