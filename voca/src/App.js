b204ff16535be4e489551aa94ceb1b37404dac26
park-sm <nambawon1@naver.com>
2022-08-09 19:31:47 +0900

    voca 완성

--- voca/src/App.js
+++ voca/src/App.js
@@ -3,6 +3,8 @@ import DayList from "./component/DayList";
 import Header from "./component/Header";
 import EmptyPage from "./component/EmptyPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CreateWord from "./component/CreateWord";
+import CreateDay from "./component/CreateDay";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<DayList />} />
           <Route path="/day/:day" element={<Day />} />
+          <Route path="/create_word" element={<CreateWord />} />
+          <Route path="/create_day" element={<CreateDay />} />
           <Route path="*" element={<EmptyPage />} />
         </Routes>
       </div>
